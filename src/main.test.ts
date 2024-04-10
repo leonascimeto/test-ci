31c91052d29bbc5aa128b8112433f244a223e2cd
leo <nascimentoleo899@gmail.com>
2024-04-10 15:33:40 -0300

    feat: add sub calculate

--- src/main.test.ts
+++ src/main.test.ts
@@ -1,4 +1,4 @@
-import { soma } from './main';
+import { soma, subtracao } from './main';
 
 test('should sum 2 numbers', () => {
   const input = {
@@ -7,6 +7,17 @@ test('should sum 2 numbers', () => {
   };
 
   const sut = soma(input.a, input.b);
+
+  expect(sut).toBe(3);
+});
+
+test('should sub 2 numbers', () => {
+  const input = {
+    a: 5,
+    b: 2,
+  };
+
+  const sut = subtracao(input.a, input.b);
 
   expect(sut).toBe(3);
 });
